e20a47ad1d600e60c100153023f351f5386933c3
MaarianaaPa <reyes.palaciosmariana606@gmail.com>
2023-02-22 19:58:36 -0600

    first commit

---
+++ Tarea3POO.java
@@ -0,0 +1,19 @@
+package unam.fes.poo.tarea3poo;
+
+public class Tarea3POO {
+
+    public static void main(String[] args) {
+        int repetir;
+        for (repetir = 1; repetir <= 3; repetir++) {
+            System.out.printf("%7s %6s %n", "|", "|");
+        }
+        System.out.println("______ ______ ______");
+        for (repetir = 1; repetir <= 3; repetir++) {
+            System.out.printf("%7s %6s %n", "|", "|");
+        }
+        System.out.println("______ ______ ______");
+        for (repetir = 1; repetir <= 3; repetir++) {
+            System.out.printf("%7s %6s %n", "|", "|");
+        }
+    }
+}
